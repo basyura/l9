578cf7ec93c15b0858d8ab47e7d0697ada643371
basyura <basyura@gmail.com>
2013-09-03 22:06:17 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+L9::Application.config.secret_key_base = '748b900e447c8d5d0244e3ca3d4cd1d32e20ddb79ac4372d60ee123a2fbd57b30e9f33bd373359e5325297b5f4a3e96cf6485ca096ed22e4543c6c21fd3d3742'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+L9::Application.config.session_store :cookie_store, key: '_l9_session'
