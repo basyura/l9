576c29707821a0a3dc9fa2188e08066a4f346bd7
basyura <basyura@gmail.com>
2013-09-04 21:56:18 +0900

    parse param

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -5,6 +5,12 @@ class ApiController < ApplicationController
     puts params[:date]
     puts params[:message]
 
+    Log.new({
+      :username => params[:username],
+      :date     => Time.parse(params[:date]),
+      :message  => params[:message]
+    }).save
+
     render :json => 'ok'
   end
 end
--- lib/sender.rb
+++ lib/sender.rb
@@ -3,8 +3,8 @@ require 'net/http'
 uri = URI.parse("http://localhost:3000/api/create.json")
 response = Net::HTTP.post_form(uri, {
   "username" => "basyura",
-  "date"     => "2013-09-04",
-  "message"  => "<hoge>aaa</hoge>",
+  "date"     => "2013-09-04 12:10",
+  "message"  => "ヽ(｀Д´)ﾉ",
 })
 
 puts response.body
