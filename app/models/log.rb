6e8d2dac1b8f7032aa2ea33d76a4f066fe608461
basyura <basyura@gmail.com>
2013-09-07 10:22:48 +0900

    reverse logs

--- app/models/log.rb
+++ app/models/log.rb
@@ -3,6 +3,6 @@ class Log < ActiveRecord::Base
     date = date_string && date_string.length >= 6 ? Time.parse(date_string[0,6] + "01") : Time.now
     st = Time.local(date.year, date.month, 1, 0, 0, 0)
     ed = date.end_of_month
-    self.where('? <= date and date <= ?', st, ed).order(:date).all
+    self.where('? <= date and date <= ?', st, ed).order(:date => :desc).all
   end
 end
